b3cb69fb407d57577d000818387029a5d4595aa2
Gleb <surkog08@gmail.com>
2021-10-02 21:37:12 +0300

    Add Logo

--- src/components/header/HeaderInner.jsx
+++ src/components/header/HeaderInner.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import HeaderLogo from "./HeaderLogo";
 import Nav from "./Nav";
+import ImageSrc from "../../img/logo.png"
 
 const HeaderInner = () => {
     return (
         <div className = "header_inner">
-            <HeaderLogo logo = "gotoexam"/>
+            <HeaderLogo logo = {{text:"gotoexam",img:ImageSrc}}/>
             <Nav/>
         </div>
     );
--- src/components/header/HeaderLogo.jsx
+++ src/components/header/HeaderLogo.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 const HeaderLogo = (props) => {
     return (
         <div className = "header_logo">
-            {props.logo}
+            {props.logo.text}
+            <div>
+                <img className="logo_img" src={props.logo.img}/>
+            </div>
         </div>
     );
 };
