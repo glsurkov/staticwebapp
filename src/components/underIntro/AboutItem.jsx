9965e6064c269bfc394a2ceb767630cf0b3c1356
Gleb <surkog08@gmail.com>
2021-10-03 18:12:21 +0300

    drop info script

--- src/components/underIntro/AboutItem.jsx
+++ src/components/underIntro/AboutItem.jsx
@@ -1,8 +1,25 @@
-import React from 'react';
+import React, {useRef} from 'react';
 
 const AboutItem = (props) => {
+
+    const clickRef = useRef();
+
+    const active_drop_info = (e) =>
+    {
+        e.preventDefault();
+        if (clickRef.current.lastChild.classList.contains("drop_info_active")) {
+            clickRef.current.lastChild.classList.remove('drop_info_active');
+            clickRef.current.lastChild.classList.add('drop_info');
+        }
+        else {
+            clickRef.current.lastChild.classList.add('drop_info_active');
+            clickRef.current.lastChild.classList.remove('drop_info');
+        }
+    }
+
+
     return (
-        <div className = "about_item">
+        <div onClick = {active_drop_info} ref = {clickRef} className = "about_item">
             <div className="about_image">
                 <img src={props.item.img} />
             </div>
